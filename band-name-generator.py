6ae59a20c70db154a2786624426d8be920b7ff8d
Andy Ferris <aawferris@aawferris-2.local>
2022-03-31 14:24:01 -0400

    adding this repo for the first time

---
+++ band-name-generator.py
@@ -0,0 +1,11 @@
+# 1. Create a greeting for your program.
+
+# 2. Ask the user for the city that they grew up in.
+city = (input("Welcome! We're going to help out your general rockitude by helping you name your band!  First, what city were you born in? \n"))
+# 3. Ask the user for the name of a pet.
+pet_name = (input("What is the name of your pet? \n"))
+# 4. Combine the name of their city and pet and show them their band name.
+rock_band_name = city + " " + pet_name
+print("Your band name is " + rock_band_name + "!  Rock out!")
+# 5. Make sure the input cursor shows on a new line, see the example at:
+#   https://replit.com/@appbrewery/band-name-generator-end
---
+++ hp.py
@@ -0,0 +1,16 @@
+adj1 = input("Adjective: ")
+adj2 = input("Adjective: ")
+adj3 = input("Adjective: ")
+verb1 = input("Past Tense Verb: ")
+verb2 = input("Past Tense Verb: ")
+verb3 = input("Verb: ")
+person1 = input("Person: ")
+person2 = input("Person: ")
+noun1 = input("Noun:")
+pluralnoun = input("Plural noun: ")
+
+madlib = f"Let me tell you about the time {person1} put their {adj1} {noun1} in the {adj2} toilet.  It was so {adj3}!! \
+When {person2} saw it, they {verb1} outta there!  {person1}'s feet didn't fail them as they grabbed all of the \
+{pluralnoun} and {verb2} all the way home. They will never {verb3} again."
+
+print(madlib)
